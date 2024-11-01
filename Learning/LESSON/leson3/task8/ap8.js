e22b34dc484497175daa7a63fb2ac60b595bed8b
DAR13-A <159921806+DAR13-A@users.noreply.github.com>
2024-11-01 15:58:22 +0200

    task9

---
+++ Learning/LESSON/leson3/task8/ap8.js
@@ -0,0 +1,7 @@
+const start4 = 1;
+const end4 = 15;
+let sum = 0;
+for (let i = start4; i <= end4; i++) {
+  sum += i;
+}
+console.log("Сума чисел від 1 до 15:", sum);
---
+++ Learning/LESSON/leson3/task9/ap9.js
@@ -0,0 +1,7 @@
+const start5 = 15;
+const end5 = 35;
+let product = 1;
+for (let i = start5; i <= end5; i++) {
+  product *= i;
+}
+console.log("Добуток чисел від 15 до 35:", product);
